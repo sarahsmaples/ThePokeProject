6df0f919b5fa7dbc47e0f8458a5481e453471b40
sarahsmaples <sarah@tallymeals.com>
2026-02-12 23:56:55 -0600

    Gitting ready

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 
   return {
+    pathPrefix: process.env.PATH_PREFIX || "/",
     dir: {
       input: "src",
       includes: "_includes",
